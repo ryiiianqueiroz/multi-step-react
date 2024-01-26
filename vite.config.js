8c93816715f19b7c8b017b6ead7c9e6af83f0da8
ryiiianqueiroz <ryanqueiroz2017@gmail.com>
2024-01-26 00:49:23 -0300

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/multi-step-react",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/multi-step-react',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
